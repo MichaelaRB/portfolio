4aa97754a53ca11f8fdcf6a87ed58d2f5fbaf1ce
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-14 16:41:45 -0500

    Added information to the resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,8 +2,25 @@ export default function Blog() {
     return (
       <div>
         <h1>Resume</h1>
+        <h2>Front End Proficiencies</h2>
         <ul>
-            <li></li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>Responsive Design</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>React</li>
+        </ul>
+        <h2>Back End Proficiencies</h2>
+        <ul>
+            <li>APIs</li>
+            <li>REST</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL</li>
         </ul>
       </div>
     );
