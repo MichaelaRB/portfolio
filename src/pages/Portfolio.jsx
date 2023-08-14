fd498599d601118081ae5784ff6c183f5bbd32cf
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-14 12:44:49 -0500

    added links and images for the portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,18 @@ export default function Blog() {
     return (
       <div>
         <h1>Portfolio</h1>
+        <a href="https://michaelarb.github.io/pet-facts/"><img src="../assets/petFacts.png"></img></a>
+        <a href="https://brewreview-c780e18c5286.herokuapp.com/"><img src="../assets/brewReview.png"></img></a>
+        <a href="https://michaelarb.github.io/weather-dashboard/"><img src="../assets/weather.png"></img></a>
+        <a href="https://michaelarb.github.io/daily-schedule/"><img src="../assets/scheduler.png"></img></a>
+        <a href="https://michaelarb.github.io/password-generator/"><img src="../assets/passwordGen.png"></img></a>
+
+
+        <a href="https://github.com/MichaelaRB/weather-dashboard"></a>
+        <a href="https://github.com/MichaelaRB/pet-facts"></a>
+        <a href= "https://github.com/JohnDallasMoore/BrewReview"></a>
+        <a href= "https://github.com/MichaelaRB/daily-schedule"></a>
+        <a href= "https://github.com/MichaelaRB/password-generator"></a>
       </div>
     );
   }
